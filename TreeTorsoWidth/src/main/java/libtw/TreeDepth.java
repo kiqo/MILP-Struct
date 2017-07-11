6b7566a53de12ea060fcee3900fc3d343607cb61
Verena <valentinelee@freenet.de>
2017-07-11 15:36:26 +0200

    use deep copy of graph for TreeDepth algorithm, use maxDegree heuristic in TreeDepth algorithm

--- TreeTorsoWidth/src/main/java/libtw/TreeDepth.java
+++ TreeTorsoWidth/src/main/java/libtw/TreeDepth.java
@@ -1,5 +1,6 @@
 package main.java.libtw;
 
+import nl.uu.cs.treewidth.algorithm.GreedyFillIn;
 import nl.uu.cs.treewidth.algorithm.LowerBound;
 import nl.uu.cs.treewidth.algorithm.UpperBound;
 import nl.uu.cs.treewidth.input.GraphInput;
@@ -13,18 +14,32 @@ import org.slf4j.LoggerFactory;
 import java.util.*;
 
 /**
- * A lower bound for the tree-depth is the logarithm of the length of any path in the
- * graph, more exactly for a Path Pn with length n:
- * td(Pn) = roundUp(log2(n + 1))
+ * The tree-depth td(G) is defined to be the minimum height of a rooted forest F
+ * such that G is a subgraph of the closure of F, where the closure of F is obtained
+ * by adding edges between ancestor and descendents in a tree.
+ *
+ * The treewidth is a lower bound for treedepth. Another lower bound is the logarithm
+ * of the length of any path in the graph.
+ *
+ * For upper bound, the idea is to select some starting points and run a DFS from these.
+ * These starting points can be chosen randomly or with greedy heuristic (like taking
+ * the center of some greedily constructed maximal path in the graph).
  *
  * @author Verena Dittmer
  * */
 public class TreeDepth<D extends GraphInput.InputData> implements LowerBound<D>, UpperBound<D> {
+
+    class MyConverter<D extends GraphInput.InputData> implements NGraph.Convertor<D,LPInputData> {
+        public LPInputData convert(NVertex<D> old ) {
+            LPInputData d = new LPInputData(old.data.id, old.data.name, false);
+            d.setNodeHandled(false);
+            return d;
+        }
+    }
+
     private static final Logger LOGGER = LoggerFactory.getLogger(TreeDepth.class);
-    // times that some random path is generated
-    private static final int NUM_ITERATIONS_LB = 1000;
-    // times that some DFS spanning tree is generated, should be smaller than NUM_ITERATIONS_LB
-    private static final int NUM_ITERATIONS_UB = 100;
+    // times that some DFS spanning tree are generated
+    private static final int NUM_ITERATIONS = 100;
     private int lowerBound = Integer.MIN_VALUE;
     private int upperBound = Integer.MAX_VALUE;
 
@@ -55,7 +70,7 @@ public class TreeDepth<D extends GraphInput.InputData> implements LowerBound<D>,
 
     @Override
     public void setInput(NGraph<D> g)  {
-        this.graph = g;
+        graph = g.copy(new MyConverter());
     }
 
     @Override
@@ -63,7 +78,7 @@ public class TreeDepth<D extends GraphInput.InputData> implements LowerBound<D>,
         // Lower bound of tree depth
         int maxPathLength = Integer.MIN_VALUE;
 
-        for (int i = 0; i < NUM_ITERATIONS_LB; i++) {
+        for (int i = 0; i < NUM_ITERATIONS; i++) {
             List<ListVertex<D>> path = findRandomPath();
             int pathLength = path.size();
             if (pathLength > maxPathLength) {
@@ -71,24 +86,19 @@ public class TreeDepth<D extends GraphInput.InputData> implements LowerBound<D>,
                 maxPathLength = pathLength;
             }
 
-            if (i % (NUM_ITERATIONS_LB/NUM_ITERATIONS_UB) == 0) {
-                // try to get better upper bound
-                // construct a DFS by using the node in the middle of the longest path as root
-                // TODO do it for longest path as well?
-                ListVertex<D> rootNode = path.get(path.size() / 2);
-                Set<ListVertex<D>> nodesHandled = new HashSet();
-                int height = DFSTree(rootNode, nodesHandled);
-                // System.out.println("   " + height);
-                if (nodesHandled.size() != graph.getNumberOfVertices()) {
-                    // graph is not connected, the obtained result is not valid
-                    height = Integer.MAX_VALUE;
-                }
+            // try to get better upper bound
+            // construct a DFS by using the node in the middle of the longest path as root
+            ListVertex<D> rootNode = path.get(path.size() / 2);
+            Set<ListVertex<D>> nodesHandled = new HashSet();
+            int height = DFSTreeByMaxDegreeRoot(rootNode, nodesHandled);
 
+            if (nodesHandled.size() != graph.getNumberOfVertices()) {
+                // graph is not connected, the obtained result is not valid
+                height = Integer.MAX_VALUE;
+            }
 
-
-                if (height < upperBound) {
-                    this.upperBound = height;
-                }
+            if (height < upperBound) {
+                this.upperBound = height;
             }
         }
         lowerBound = (int) Math.ceil(Math.log(maxPathLength + 1) / Math.log(2.0));
@@ -150,6 +160,46 @@ public class TreeDepth<D extends GraphInput.InputData> implements LowerBound<D>,
         }
 
         return pathFound;
+    }
+
+
+    /*
+     * Input is the root node, returns distance from the lowest descendant of the rootNode, i.e. the current
+     * height of the tree
+     * Starts by constructing first the subtree for the neighbour of the rootNode with maximal degree in the graph
+     */
+    private int DFSTreeByMaxDegreeRoot(ListVertex<D> rootNode, Set<ListVertex<D>> handledVertices) {
+
+        handledVertices.add(rootNode);
+        // System.out.print(rootNode.data.name + " ");
+
+        int height = 0;
+        int maxDegree;
+        NVertex<D> maxDegreeVertex;
+
+        while (true) {
+            maxDegree = Integer.MIN_VALUE;
+            maxDegreeVertex = null;
+            for (NVertex<D> neighbor : rootNode.neighbors) {
+                if (!handledVertices.contains(neighbor) && maxDegree < neighbor.getNumberOfNeighbors()) {
+                    maxDegreeVertex = neighbor;
+                    maxDegree = neighbor.getNumberOfNeighbors();
+                }
+            }
+
+            // all neighbours of current root node already in tree
+            if (maxDegreeVertex == null) {
+                break;
+            }
+
+            // take maxDegreeVertex as root for subtree
+            int heightSubtree = DFSTreeByMaxDegreeRoot((ListVertex<D>) maxDegreeVertex, handledVertices);
+            if (heightSubtree > height) {
+                height = heightSubtree;
+            }
+        }
+
+        return ++height;
     }
 
 
